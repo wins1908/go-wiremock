9f867bdaa4e3cc0f501702e8b7a1825ca9d71836
wins1908 <wins1908@gmail.com>
2022-03-05 01:03:34 +0800

    added TestIDJSONField

--- test_id.go
+++ test_id.go
@@ -8,34 +8,41 @@ import (
 )
 
 const (
-	// TestIDHeader ...
-	TestIDHeader = "X-Wiremock-Test-ID"
+	// TestIDRequestHeader ...
+	TestIDRequestHeader = "X-Wiremock-Test-ID"
+	// TestIDJSONField ...
+	TestIDJSONField = "wiremock_test_id"
 
 	testIDCtxKey = "test_id"
 )
 
 // ContextForTest set test ID into context
 func ContextForTest(ctx context.Context, t *testing.T) context.Context {
-	return context.WithValue(ctx, testIDCtxKey, CreateTestID(t))
+	return ContextWithTestID(ctx, CreateTestID(t))
 }
 
 // RequestForTest ...
 func RequestForTest(req *http.Request, t *testing.T) *http.Request {
-	req.Header.Set(TestIDHeader, CreateTestID(t))
+	req.Header.Set(TestIDRequestHeader, CreateTestID(t))
 	return req
 }
 
 // HeaderForTest ...
 func HeaderForTest(headers map[string]interface{}, t *testing.T) {
-	headers[TestIDHeader] = CreateTestID(t)
+	headers[TestIDJSONField] = CreateTestID(t)
+}
+
+// ContextWithTestID set test ID into context
+func ContextWithTestID(ctx context.Context, testID string) context.Context {
+	return context.WithValue(ctx, testIDCtxKey, testID)
 }
 
 // TestIDToContextMiddleware ...
 func TestIDToContextMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		ctx := req.Context()
-		if testID := req.Header.Get(TestIDHeader); testID != "" {
-			req = req.WithContext(context.WithValue(ctx, testIDCtxKey, testID))
+		if testID := req.Header.Get(TestIDRequestHeader); testID != "" {
+			req = req.WithContext(ContextWithTestID(ctx, testID))
 		}
 
 		next.ServeHTTP(w, req)
@@ -46,7 +53,7 @@ func TestIDToContextMiddleware(next http.Handler) http.Handler {
 func TestIDToOutgoingRequestHeaderMiddleware(next http.RoundTripper) http.RoundTripper {
 	return roundTripperFunc(func(req *http.Request) (*http.Response, error) {
 		if testID, ok := req.Context().Value(testIDCtxKey).(string); ok && testID != "" {
-			req.Header.Set(TestIDHeader, testID)
+			req.Header.Set(TestIDRequestHeader, testID)
 		}
 		return next.RoundTrip(req)
 	})
@@ -58,6 +65,7 @@ func (fn roundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) 
 	return fn(req)
 }
 
+// CreateTestID ...
 func CreateTestID(t *testing.T) string {
 	return fmt.Sprintf("%s:%p", t.Name(), t)
 }
